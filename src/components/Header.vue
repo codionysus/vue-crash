0165476cbf6612a01790c5702107fd484401540a
Denis <denis.kovacevic.95@gmail.com>
2021-04-20 22:00:33 +0200

    inital

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button @click="onClick()" text="Add Task" color="green"/>
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+    },
+    components:{
+        Button
+    },
+    methods:{
+        onClick(){
+            console.log("click");
+        }
+    }
+}
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
